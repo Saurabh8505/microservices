ce2e6bdb3f7dd1f8d83db97f3880195cb5e6c5d6
Saurabh Mishra <103993107+Saurabh8505@users.noreply.github.com>
2025-02-24 16:04:50 +0530

    added Dependency Inversion Principle

---
+++ src/com/d/Test.java
@@ -0,0 +1,13 @@
+package com.d;
+
+public class Test {
+	 public static void main(String[] args) {
+	        
+	        Switch lightSwitch = new Switch(new LightBulb());
+	        lightSwitch.press(); 
+	        lightSwitch.press(); 
+	        Switch fanSwitch = new Switch(new Fan());
+	        fanSwitch.press(); 
+	        fanSwitch.press(); 
+	    }
+}
